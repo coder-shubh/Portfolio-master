88732b8d00e8693da1addcb06d92ffbc75f1e48f
coder-shubh <shubhamsingh00222.ss@gmail.com>
2024-09-06 05:01:50 +0530

    header update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,7 +39,9 @@ function NavBar() {
     >
       <Container>
         <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
+        <strong className="main-name">SKS</strong>
+
+          {/* <img src={logo} className="img-fluid logo" alt="brand" /> */}
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
